73738483767e875637d9c27f2fe9eacc9ad21296
arya-ht <aryaht23@gmail.com>
2017-07-18 22:34:37 -0500

    error calculation
    This program calculates the absolute and relative error of a derivative using the forward formula with by an error of order O(h).

---
+++ CalcError.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#define DF(k,x,h) fabs((k*cos(k*x))-((sin(k*(x+h))-(sin(k*x)))/h))
+#pragma warning(disable:4996)
+int main() {
+	double j, k = 0, h = 0.0, N = 0, x = 0;
+	double absErr = 0.0, relErr;
+	printf("Enter k: ");
+	scanf("%lf", &k);
+	printf("Enter N: ");
+	scanf("%lf", &N);
+	h = 1 / N;
+	for (j = 0; j <= N; j++) {
+		x = j*h;
+		absErr += DF(k,x,h);
+	}
+	relErr = absErr / k;
+	printf("%lf\n", absErr);
+	printf("%lf\n", relErr);
+
+	system("pause");
+	return 0;
+}
