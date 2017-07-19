9b8d999ec91957626c9d25008634ecf3c57829d6
Arya H <aryaht23@gmail.com>
2017-07-18 23:05:53 -0500

    Update CalcError.c

--- CalcError.c
+++ CalcError.c
@@ -2,6 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 #define DF(k,x,h) fabs((k*cos(k*x))-((sin(k*(x+h))-(sin(k*x)))/h))
+/*
+ *created by Arya HajiTaheri
+ */
 #pragma warning(disable:4996)
 int main() {
 	double j, k = 0, h = 0.0, N = 0, x = 0;
